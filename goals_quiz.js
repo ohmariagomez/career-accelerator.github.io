690d20533e55e8a443d202d5c490418eab756f5f
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-09-25 19:19:47 -0400

    Update goals_quiz.js

--- goals_quiz.js
+++ goals_quiz.js
@@ -3,7 +3,6 @@ const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
 
 let questions = [
     {
