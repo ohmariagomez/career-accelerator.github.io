bf0e083c7f5d19980d9bca1df87be106aebff5a1
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-09-25 19:28:45 -0400

    Update goals_quiz.js

--- goals_quiz.js
+++ goals_quiz.js
@@ -1,9 +1,15 @@
+// select all elements
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
 
+// create our questions
 let questions = [
     {
         question : "Have better time management",
@@ -13,13 +19,17 @@ let questions = [
     }
 ];
 
+// create some variables
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
+const questionTime = 10; // 10s
 const gaugeWidth = 150; // 150px
 const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
 let score = 0;
 
+// render a question
 function renderQuestion(){
     let q = questions[runningQuestion];
     
@@ -37,11 +47,58 @@ function startQuiz(){
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
 }
 
 // render progress
 function renderProgress(){
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter render
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnwer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
     }
 }
