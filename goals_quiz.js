c5369248acf46af1046761e904d34eae39df0576
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-09-25 19:13:21 -0400

    Update goals_quiz.js

--- goals_quiz.js
+++ goals_quiz.js
@@ -1,1 +1,48 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
 
+let questions = [
+    {
+        question : "Have better time management",
+        choiceA : "Growth",
+        choiceB : "Checklist",
+        correct : "A"
+    }
+];
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let score = 0;
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+}
+
+start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+}
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
