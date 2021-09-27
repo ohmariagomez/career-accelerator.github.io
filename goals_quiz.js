0476a532fa25a6489a68df185492e582c31a0b29
ohmariagomez <78579607+ohmariagomez@users.noreply.github.com>
2021-09-27 14:47:17 -0400

    Update goals_quiz.js

--- goals_quiz.js
+++ goals_quiz.js
@@ -2,16 +2,14 @@
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
+const choices = document.getElementById("choices");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
+const choiceResponse = document.getElementById("choiceResponse");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 const scoreMessage = document.getElementById("scoreMessage");
 const quizAgain = document.getElementById("quizAgain");
-const choiceResponse = document.getElementById("choiceResponse");
-const choices = document.getElementById("choices");
 
 // create our questions
 let questions = [
@@ -98,10 +96,6 @@ let questions = [
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
 let score = 0;
 
 // render a question
@@ -123,8 +117,6 @@ function startQuiz(){
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
 }
 
 // render progress
@@ -134,34 +126,12 @@ function renderProgress(){
     }
 }
 
-// counter render
-
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            scoreRender();
-        }
-    }
-}
-
 // checkAnswer
-
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
         // answer is correct
         score++;
-        // display correct
+        // display correct response
         choices.style.display = "none";
         choiceResponse.innerHTML= "<p><b>Correct!</b></p>" +"<p>"+ questions[runningQuestion].response+"</p>";
         choiceResponse.style.display = "block";
@@ -171,7 +141,7 @@ function checkAnswer(answer){
         
     }else{
         // answer is wrong
-        // display wrong!
+        // display wrong response
         choices.style.display = "none";
         choiceResponse.innerHTML= "<p><b>Incorrect!</b></p>"+"<p>"+questions[runningQuestion].response+"</p>";
         choiceResponse.style.display = "block";
@@ -204,11 +174,9 @@ function answerIsWrong(){
 function scoreRender(){
     quiz.style.display = "none";
     scoreDiv.style.display = "block";
-    scoreDiv.innerHTML = "<p> You scored " + score + " out of 11!</p>";
+    scoreDiv.innerHTML = "<p> You scored " + score + " out of" + questions.length + "!!</p>";
     
-    // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
-    
+    // classify scores into categories  
     if (score < 4) {
         scoreMessage.innerHTML = "<p>Try reading through our growth poster section and try again!</p>";
     }
